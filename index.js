d9525812801cad06ddda5731d2e98b59299c0365
anayetabir <anayetabir007@gmail.com>
2024-01-17 05:13:06 +0600

    Server Connceted

--- index.js
+++ index.js
@@ -34,6 +34,21 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const eventCollection = client.db('eventDB').collection('event');
+
+        app.post('/event', async (req, res) => {
+
+            const newEvent = req.body;
+            console.log(newEvent);
+            const result = await eventCollection.insertOne(newEvent);
+            res.send(result);
+        })
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
